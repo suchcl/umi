3553f391f47a1cae7f80ecdae57a632aba44d3fc
suchcl <ya5969@163.com>
2022-02-11 13:16:35 +0800

    antd引入

--- umi1/src/pages/picture/index.tsx
+++ umi1/src/pages/picture/index.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import {Button,Upload,Popconfirm} from "antd";
+import {UploadOutlined} from "@ant-design/icons";
 import style from "./pic.less";
 
 // 箭头函数形式的组件
@@ -9,11 +11,23 @@ export default () => {
             <div className="box">
                 <h4>{msg}</h4>
                 <div className={style.pic_area}>
-                    <div className="pic">
+                    <div className={style.pic}>
                         {/* 导入svg */}
                         <img src={require("./images/i1.svg")} alt="" />
                     </div>
                     <div className="text"></div>
+                </div>
+                <div className={style.btn_area}>
+                    <Button type="primary">Button</Button>
+                    <Upload>
+                        <Button>
+                            <UploadOutlined />上传
+                        </Button>
+                    </Upload>
+                    {/* 确认按钮，有提示信息 */}
+                    <Popconfirm title="确认要删除？" okText="确认" cancelText="取消">
+                        <Button>确认</Button>
+                    </Popconfirm>
                 </div>
             </div>
         </div>
