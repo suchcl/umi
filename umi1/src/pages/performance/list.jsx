4cc6dac8aa96107f60f590adabca817f06915521
suchcl <ya5969@163.com>
2022-03-02 16:25:40 +0800

    代码调优

--- umi1/src/pages/performance/list.jsx
+++ umi1/src/pages/performance/list.jsx
@@ -1,8 +1,7 @@
-// import { nanoid } from "nanoid";
 import Nav from "../../components/nav";
-import { useState, memo, useCallback } from "react";
+import { memo, useState, useCallback } from "react";
 
-// 创建模拟数据
+// 使用包含增加项的元素创建模拟数据
 const data = new Array(100)
     .fill()
     .map((_, i) => i + 1)
@@ -11,10 +10,11 @@ const data = new Array(100)
         name: `Item ${n}`
     }));
 
-export default () => {
-    // 包含所选项的数组
+export default function App() {
+    // 包含所选item的数组
     const [selected, setSelected] = useState([]);
 
+    // 选择或取消选择给定的item
     const toggleItem = useCallback((item) => {
         setSelected((prevSelected) => {
             if (!prevSelected.includes(item)) {
@@ -25,24 +25,19 @@ export default () => {
         });
     }, []);
 
-
     return (
         <div className="App">
             <Nav />
-            <div className="test">
-                <h2>性能测试</h2>
-                <List data={data} selectedItems={selected} toggleItem={toggleItem} />
-            </div>
+            <h1>List Example</h1>
+            <List data={data} selectedItems={selected} toggleItem={toggleItem} />
         </div>
     );
-};
+}
 
 const List = ({ data, selectedItems, toggleItem }) => {
-    const MemoizedListItem = memo(ListItem);
     const handleClick = useCallback(
-        (item) => {  // We now receive the selected item
+        (item) => {
             toggleItem(item);
-            // console.log(item);
         },
         [toggleItem]
     );
@@ -52,7 +47,7 @@ const List = ({ data, selectedItems, toggleItem }) => {
             {data.map((item) => (
                 <MemoizedListItem
                     key={item.id}
-                    item={item}  // We pass the full item instead of the name
+                    item={item}
                     selected={selectedItems.includes(item)}
                     onClick={handleClick}
                 />
@@ -62,19 +57,26 @@ const List = ({ data, selectedItems, toggleItem }) => {
 };
 
 const ListItem = ({ item, selected, onClick }) => {
+    // 运行复杂的操作来模拟负载，在实际开发中，这可以是自定义的复杂的渲染。
     expensiveOperation(selected);
+
     return (
-        <li style={selected ? { textDecoration: 'line-through' } : undefined}
+        <li
+            style={selected ? { textDecoration: "line-through" } : undefined}
             onClick={() => onClick(item)}
         >
             {item.name}
         </li>
-    )
-}
+    );
+};
 
+const MemoizedListItem = memo(ListItem);
+
+// 运行复杂的操作来模拟负载，在实际开发中，这可以是自定义的复杂的渲染。
 const expensiveOperation = (selected) => {
+    // 这里我们使用 selected 只是因为我们要模拟一个依赖于 props 的操作
     let total = selected ? 1 : 0;
-    for (let i = 0; i < 2000; i++) {
+    for (let i = 0; i < 2000000; i++) {
         total += Math.random();
     }
     return total;
