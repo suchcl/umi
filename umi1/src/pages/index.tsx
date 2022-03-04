0f994d98cc7b664521f2c33a7ea9538eddaf3476
suchcl <ya5969@163.com>
2022-03-04 13:41:10 +0800

    axios

--- umi1/src/pages/index.tsx
+++ umi1/src/pages/index.tsx
@@ -1,28 +1,41 @@
 import styles from './index.less';
-import { Link } from 'umi';
 import Nav from "../components/nav";
+import {Button} from "antd";
+import axios from "axios";
+import { useState } from 'react';
 
 export default function IndexPage() {
+  const [id,setId] = useState(2);
+  const [user,setUser] = useState([]);
+  const getUser = (event:any) => {    
+    if(id === 1){
+      setId(2);
+    }else{
+      setId(1);
+    }
+    
+    axios.get('http://localhost:3000/', {
+      params: {
+        id: id
+      }
+    })
+    .then(function (res) {
+      console.log(res);
+    });  
+  }
+
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
-      {/* <ul className="nav">
-        <li>
-          <Link to="/list">列表页</Link>
-        </li>
-        <li>
-          <Link to="/detail">详情页</Link>
-        </li>
-        <li>
-          <Link to="/performance">性能测试</Link>
-        </li>
-      </ul> */}
       <Nav />
-      
+      <div className="http">
+        <Button type='primary' onClick={(event) => getUser(event)}>{id}</Button>
+      </div>
       <div className="user-center">
         <a href="/user">大个人中心</a>
         <br />
         <div className="text">
+          <h4>再来一个</h4>
           <p className='t1'>我国警告见效了？当立外长的面，澳外长向华示好：坚持一中原则</p>
           <p className='t2'>杜月笙虽是青帮老大，却有一个闻名于世的儿子，一生为祖国奋斗</p>
           <p className='t3'>百万订单“考验”父子情，冰墩墩工厂老板：儿子哭着要，莫法</p>
