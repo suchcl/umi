16de42a8eb67e0e24783afdc746556174d1c0146
suchcl <ya5969@163.com>
2023-02-20 17:41:58 +0800

    移动端适配

--- umi2/.umirc.ts
+++ umi2/.umirc.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from 'umi';
+const px2rem = require("postcss-px2rem-exclude");
 
 export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  layout:{},
+  // layout:{},
   routes: [
     { path: '/', component: '@/pages/index' },
   ],
   fastRefresh: {},
-  mfsu:{}
+  mfsu: {},
+  extraPostCSSPlugins: [
+    px2rem({
+      remUnit: 75,
+      exclude: '/node_modules/i'
+    })
+  ]
 });
--- umi2/src/pages/index.tsx
+++ umi2/src/pages/index.tsx
@@ -1,9 +1,14 @@
 import styles from './index.less';
+import "lib-flexible";
+import logo from "@/assets/images/logo.png";
 
 export default function IndexPage() {
   return (
-    <div>
-      <h1 className={styles.title}>Page index</h1>
-    </div>
+    <>
+      {/* <img src={logo} alt="logo" className={styles.logo} />
+      <h1 className={styles.title}>Page index</h1> */}
+      {/* 严选经纪人 */}
+      <div className={styles.agent}></div>
+    </>
   );
 }
